57d28d63a9e894eebfbc8187457d929c2b5af906
oron cohen <46217866+MaybeOron@users.noreply.github.com>
2022-03-31 15:11:03 +0300

    Update script.sh

--- .github/workflows/dir/script.sh
+++ .github/workflows/dir/script.sh
@@ -2,5 +2,5 @@
 echo "this cluster is from github actions: $cluster"
 echo "this service is from github actions: $service"
 echo "this variable is from github actions: $bool"
-haha="haha"
+export haha="haha"
 echo "$haha is inside the script"
